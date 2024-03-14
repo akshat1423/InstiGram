47a5f0a0ae25f37f31bbcd38af1a254e94a4fce3
Aryamaan-295 <147324149+Aryamaan-295@users.noreply.github.com>
2024-03-14 20:59:29 +0530

    create and edit bug fix

--- InstiGram-Frontend/src/pages/CreatePost/CreatePost.jsx
+++ InstiGram-Frontend/src/pages/CreatePost/CreatePost.jsx
@@ -74,7 +74,7 @@ export default function CreatePost() {
                         data,
                     ]);
                 }
-                // navigate('/feed')
+                navigate('/feed')
             })
     }
 
