36b4d0eb5d5a2fd5f8ee05e138e1b0f04d6fca25
Aryamaan-295 <147324149+Aryamaan-295@users.noreply.github.com>
2024-03-19 14:43:54 +0530

    search routing

--- InstiGram-Frontend/src/components/SearchShow/SearchShow.jsx
+++ InstiGram-Frontend/src/components/SearchShow/SearchShow.jsx
@@ -3,12 +3,14 @@ import './SearchShow.css';
 import { Link, useLocation } from "react-router-dom";
 
 export default function SearchShow(props) {
+  const location = useLocation();
+
   return (
     <div className="below-search">      
       <ul className="search-res">
         {props.result.map(result => (
             <Link to={`/profile/${result.userId}`} state={{background: location}} className="res">
-            <li key={result.userId}>{result.userName} {result.gradYear}</li>
+              <li key={result.userId}>{result.data}</li>
             </Link>
         ))}
       </ul>        
