677ef221b688bdfca0301c9be243e70748bde91c
akshat1423 <125379408+akshat1423@users.noreply.github.com>
2024-03-28 11:35:06 +0530

    Update App.jsx

--- InstiGram-Frontend/src/App.jsx
+++ InstiGram-Frontend/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 export default App
 
 
-export const BASE_URL = 'http://localhost:8000';
+export const BASE_URL = 'https://instigramiitb.pythonanywhere.com';
 
 export function getCookie(name) {
   const value = `${document.cookie}`;
